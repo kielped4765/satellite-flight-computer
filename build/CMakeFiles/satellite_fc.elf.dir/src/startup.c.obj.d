8dcd6201d1fb95d9a6b9070fcd3144444e24df42
Kiel Pedersen <pedersenkiel@gmail.com>
2026-08-19 21:24:47 -0500

    Remove build directory and add .gitignore

--- build/CMakeFiles/satellite_fc.elf.dir/src/startup.c.obj.d
+++
@@ -1,9 +0,0 @@
-CMakeFiles/satellite_fc.elf.dir/src/startup.c.obj: \
- C:/Users/peder/satellite-flight-computer/src/startup.c \
- C:/arm-toolchain/lib/gcc/arm-none-eabi/15.2.1/include/stdint.h \
- C:/arm-toolchain/arm-none-eabi/include/stdint.h \
- C:/arm-toolchain/arm-none-eabi/include/machine/_default_types.h \
- C:/arm-toolchain/arm-none-eabi/include/sys/features.h \
- C:/arm-toolchain/arm-none-eabi/include/_newlib_version.h \
- C:/arm-toolchain/arm-none-eabi/include/sys/_intsup.h \
- C:/arm-toolchain/arm-none-eabi/include/sys/_stdint.h
